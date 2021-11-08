864daf2a0ca88a6a3a644523c9e6453391c09cff
unknown <mistymoni5050@gmail.comgit config --global user.name sangita-das>
2021-11-08 13:47:01 +0600

    add get functionality

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const { MongoClient } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 app.use(cors());
+app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hyeto.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -19,6 +20,33 @@ async function run() {
   try {
     await client.connect();
     console.log('database connected successfully');
+    const database = client.db('doctors_portal');
+    const appointmentsCollection = database.collection('appointments');
+
+
+
+    app.get('/appointments', async (req, res) => {
+      const email = req.query.email;
+      const date = new Date(req.query.date).toLocaleDateString();
+      console.log(email);
+      const query = { "initialInfo.email": email, date: date }
+
+      console.log(query);
+
+      const cursor = appointmentsCollection.find(query);
+      const appointments = await cursor.toArray();
+      res.json(appointments);
+    })
+
+
+    app.post('/appointments', async (req, res) => {
+      const appointment = req.body;
+      console.log(appointment);
+      const result = await appointmentsCollection.insertOne(appointment);
+      console.log(result)
+      // res.json({ message: 'hello' })
+      res.json(result)
+    });
   }
   finally {
     // await client.close();
@@ -34,3 +62,13 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`listening at ${port}`)
 })
+
+
+
+// app.get('/users')
+// app.post('/users')
+// app.get('/users/:id')
+// app.put('/users/:id');
+// app.delete('/users/:id')
+// users: get
+// users: post
